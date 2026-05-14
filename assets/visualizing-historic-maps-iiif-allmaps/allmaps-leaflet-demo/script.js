b9a401ccc4456fa185543450ed89d371c1e86599
Anisa Hawes <87070441+anisa-hawes@users.noreply.github.com>
2026-05-14 15:05:29 +0100

    Upload /assets/visualizing-historic-maps-iiif-allmaps
    Upload assets for new EN OR lesson

---
+++ assets/visualizing-historic-maps-iiif-allmaps/allmaps-leaflet-demo/script.js
@@ -0,0 +1,31 @@
+// map setup functions
+
+const map = L.map("map", {
+  center: [42.3518, -71.05],
+  zoom: 13,
+  minZoom: 7,
+  maxZoom: 24,
+  zoomControl: false,
+});
+
+// make and load base map
+
+let tileLayerDetails = {
+  tileSize: 512,
+  zoomOffset: -1,
+  minZoom: 14,
+  maxZoom: 24,
+  crossOrigin: true,
+};
+let streets_base = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", tileLayerDetails).addTo(map);
+
+// define and load default warped maps
+
+let annotationUrl = 'https://annotations.allmaps.org/manifests/cfb327e4b43395e3';
+let warpedMapLayer = new Allmaps.WarpedMapLayer(annotationUrl).addTo(map);
+
+// add the layer list
+
+let base = { "OpenStreetMap": streets_base };
+let overlay = { "Allmaps overlay": warpedMapLayer };
+let layerControl = L.control.layers(base, overlay).addTo(map);
